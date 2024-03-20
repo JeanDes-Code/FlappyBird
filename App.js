7b83f0507b23bd7f38d56e98914d142a87626a27
Jean Desauw <desauwjean@gmail.com>
2024-03-20 18:30:37 +0100

    bird physics

--- App.js
+++ App.js
@@ -7,7 +7,15 @@ import {
   Easing,
   withSequence,
   withRepeat,
+  useFrameCallback,
 } from 'react-native-reanimated';
+import {
+  GestureHandlerRootView,
+  GestureDetector,
+  Gesture,
+} from 'react-native-gesture-handler';
+
+const GRAVITY = 981;
 
 const App = () => {
   const { width, height } = useWindowDimensions();
@@ -20,6 +28,18 @@ const App = () => {
   const bird = useImage(require('./assets/sprites/yellowbird-upflap.png'));
 
   const x = useSharedValue(width);
+
+  const birdY = useSharedValue(height / 3);
+  const birdYVelocity = useSharedValue(0);
+
+  useFrameCallback(({ timeSincePreviousFrame: dt }) => {
+    if (!dt) {
+      return;
+    }
+
+    birdY.value = birdY.value + (birdYVelocity.value * dt) / 1000;
+    birdYVelocity.value = birdYVelocity.value + (GRAVITY * dt) / 1000;
+  });
 
   useEffect(() => {
     x.value = withRepeat(
@@ -36,56 +56,64 @@ const App = () => {
     );
   }, []);
 
+  const gesture = Gesture.Tap().onStart(() => {
+    birdYVelocity.value = -500;
+  });
+
   const pipeOffset = 0;
 
   return (
-    <Canvas style={{ width, height }}>
-      {/* Background */}
-      <Image
-        image={backgroundImage}
-        fit='cover'
-        width={width}
-        height={height}
-      />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <GestureDetector gesture={gesture}>
+        <Canvas style={{ width, height }}>
+          {/* Background */}
+          <Image
+            image={backgroundImage}
+            fit='cover'
+            width={width}
+            height={height}
+          />
 
-      {/* Pipes */}
-      <Image
-        image={greenPipeBottom}
-        x={x}
-        y={height - 320 + pipeOffset}
-        width={103}
-        height={640}
-        fit='contain'
-      />
-      <Image
-        image={greenPipeTop}
-        x={x}
-        y={pipeOffset - 320}
-        width={103}
-        height={640}
-        fit='contain'
-      />
+          {/* Pipes */}
+          <Image
+            image={greenPipeBottom}
+            x={x}
+            y={height - 320 + pipeOffset}
+            width={103}
+            height={640}
+            fit='contain'
+          />
+          <Image
+            image={greenPipeTop}
+            x={x}
+            y={pipeOffset - 320}
+            width={103}
+            height={640}
+            fit='contain'
+          />
 
-      {/* base */}
-      <Image
-        image={base}
-        x={0}
-        y={height - 75}
-        width={width}
-        height={150}
-        fit='cover'
-      />
+          {/* base */}
+          <Image
+            image={base}
+            x={0}
+            y={height - 75}
+            width={width}
+            height={150}
+            fit='cover'
+          />
 
-      {/* bird */}
-      <Image
-        image={bird}
-        x={width / 4}
-        y={height / 2}
-        width={64}
-        height={48}
-        fit='contain'
-      />
-    </Canvas>
+          {/* bird */}
+          <Image
+            image={bird}
+            x={width / 4}
+            y={birdY}
+            width={64}
+            height={48}
+            fit='contain'
+          />
+        </Canvas>
+      </GestureDetector>
+    </GestureHandlerRootView>
   );
 };
 export default App;
